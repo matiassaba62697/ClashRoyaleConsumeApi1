fc67333c1f67389fa3bd634336a4892ba2efa1e6
matiassaba62697 <50413137+matiassaba62697@users.noreply.github.com>
2019-10-03 17:04:40 -0300

    message2

---
+++ src/app/models/clans/location.ts
@@ -0,0 +1,15 @@
+export class Location {
+    code: string
+    isCountry: boolean
+    name: string
+
+Location(    code: string,
+    isCountry: boolean,
+    name: string,
+){
+   this.code= code
+    this.isCountry=isCountry
+    this.name=name
+}
+
+}
